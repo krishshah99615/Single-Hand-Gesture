d92ca077e361e97e150f8c6df8a935637c8029b6
krishshah99615 <krishshah99615@gmail.com>
2020-08-10 20:26:46 +0530

    ADDED CROPPING SCRIPT

--- boxed_window_calc.py
+++ boxed_window_calc.py
@@ -1,16 +1,19 @@
+####################### LIBRARRIES ##########################
 import cv2
 import numpy
 import os
 
-BASE_DIR = "Dataset"
+####################### INITIALIZAING CAPTURE ##########################
+# Starting capturing
 cap = cv2.VideoCapture(0)
+# Setting height and width of webcam
 HEIGHT, WIDTH = 360, 640
 cv2.namedWindow('image')
-
-
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
 
+
+####################### Setting trackbar ##########################
 
 def nothing():
     pass
@@ -27,12 +30,17 @@ while(1):
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
 
+    # Get the values from track bar for bbox
+
     x1 = cv2.getTrackbarPos('x1', 'image')
     y1 = cv2.getTrackbarPos('y1', 'image')
     x2 = cv2.getTrackbarPos('x2', 'image')
     y2 = cv2.getTrackbarPos('y2', 'image')
 
+    # showing rectangle for visual aid
     cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+    # printing the bbox coorrdinates
     print([x1, y1, x2, y2])
 
     cv2.imshow("frame", frame)
---
+++ dataset.py
@@ -0,0 +1,83 @@
+####################### LIBRARRIES ##########################
+import cv2
+import numpy
+import os
+import argparse
+
+####################### INITIALIZAING CAPTURE ##########################
+# Name of base directory
+BASE_DIR = "Dataset"
+
+# Starting capturing
+cap = cv2.VideoCapture(0)
+
+# Setting height and width of webcam
+HEIGHT, WIDTH = 360, 640
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+
+####################### Getting arguments from command promt ##########################
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-n", "--name", required=True,
+                help="Name of the Gesture")
+args = vars(ap.parse_args())
+g_name = args["name"]
+
+# Creating drectory of gesturres name if does not exist
+g_dir = os.path.join(BASE_DIR, g_name)
+os.makedirs(g_dir, exist_ok=True)
+print("Made folder name "+str(g_name))
+
+
+####################### Starting capturing ##########################
+print("Starting Capturing .......")
+while(1):
+
+    # counter to get count of no of images saved
+    counter = 0
+
+    ret, frame = cap.read()
+
+    # avoid mirror image
+    frame = cv2.flip(frame, 1)
+
+    # bounding box from previous script
+    bbox = [38, 33, 263, 258]
+
+    # Make a rectabgle for visual help
+    cv2.rectangle(frame, (bbox[0], bbox[1]),
+                  (bbox[2], bbox[3]), (255, 0, 0), 2)
+    #[x1, y1, x2, y2]
+
+    # Cropping rectangle
+    rect = frame[bbox[1]:bbox[3], bbox[0]:bbox[2]]
+
+    # if user presses "s" saves image in that folder
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+
+        # storing dynaic name of the file
+        f_name = f"{g_name}-{counter}.jpg"
+        # writing the image
+        cv2.imwrite(f_name, rect)
+
+        print("Saving "+str(f_name))
+
+        # printing on screen the counter
+        cv2.putText(frame, f"Counter for {g_name} : {counter}",
+                    (400, 38), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0, 2))
+        # updating count
+        counter = counter+1
+
+    cv2.imshow("frame", frame)
+    #cv2.imshow("bbox", rect)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+#[38, 33, 263, 258]
